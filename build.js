fba2612c4f366dda9ba2199a8d6e27bee3747bba
Abhishek Susarla <cricketabhi@gmail.com>
2026-08-05 04:39:41 +0530

    fix: Remove rogue overlapping whatsapp button and update floating action bar whatsapp link

--- build.js
+++ build.js
@@ -105,7 +105,7 @@ trips.forEach(trip => {
     <div class="floating-action-bar">
         <a href="#" class="float-btn float-ig"><i class="fa-brands fa-instagram"></i></a>
         <a href="#" class="float-btn float-fb"><i class="fa-brands fa-facebook"></i></a>
-        <a href="#" class="float-btn float-wa"><i class="fa-brands fa-whatsapp"></i></a>
+        <a href="https://wa.me/918409358131?text=Hey%20sstravels!%20I%20want%20to%20know%20about%20upcoming%20trips." target="_blank" class="float-btn float-wa"><i class="fa-brands fa-whatsapp"></i></a>
         <a href="#" class="float-btn float-yt"><i class="fa-brands fa-youtube"></i></a>
         <button class="float-btn float-install" id="float-install-btn" title="Install App"><i class="fa-solid fa-download"></i></button>
     </div>
@@ -137,3 +137,4 @@ let scriptJs = fs.readFileSync('script.js', 'utf8');
 scriptJs = scriptJs.replace(/openTripModal\(trip\);/g, "window.location.href = 'trips/' + trip.id + '.html';");
 fs.writeFileSync('script.js', scriptJs);
 console.log('Updated script.js to navigate to trip pages');
+
