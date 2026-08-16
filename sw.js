7ca4730d311b5b02706017dca65ff4328e67a8eb
Abhishek Susarla <cricketabhi@gmail.com>
2026-08-16 10:47:55 +0530

    fix: Shift CSS animation delay to fix perceived image loading lag

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sstravels-1786850658503'; // Timestamp versioned
+const CACHE_NAME = 'sstravels-1786850658504'; // Timestamp versioned
 const ASSETS = [
     './',
     './index.html',
